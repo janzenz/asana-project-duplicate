5e50a55289933fca6798a699804aea40681add64
Janzen Zarzoso <janzen.zarzoso@gmail.com>
2017-11-25 14:34:38 +0800

    Manual id input for the workspace.

--- src/index.js
+++ src/index.js
@@ -6,42 +6,34 @@ import Asana from 'asana';
 const client = Asana.Client.create().useAccessToken(config.apiKey);
 
 client.users.me()
-	.then((user) => {
-		const userId = user.id;
-		let workspaceId;
-		
-		for (let workspace of user.workspaces) {
-			if (workspace.name === 'Better Doctor Inc.') {
-				workspaceId = workspace.id
-				break;
-			}
+.then((user) => {
+	const userId = user.id;
+
+	return client.tasks.findAll({
+		assignee: userId,
+		workspace: config.oldWorkspaceId,
+		completed_since: 'now',
+		opt_fields: 'id,name,assignee_status,completed',
+		limit: 2 // response limit
+	})
+})
+.then((response) => {
+	const stream = response.stream();
+
+	stream.on('data', (task) => {
+		if (task.assignee_status === 'today' || 
+			task.assignee_status === 'new') {
+			stream.pause();
+
+			// make an api call to migrate the tasks
+			new Promise((resolve, reject) => {
+				console.log(task);
+				stream.resume();
+			});
 		}
-
-		return client.tasks.findAll({
-			assignee: userId,
-			workspace: workspaceId,
-			completed_since: 'now',
-			opt_fields: 'id,name,assignee_status,completed',
-			limit: 2 // response limit
-		})
+	}).on('end', () => {
+		console.log('Done transferring the tasks.')
+	}).on('error', (err) => {
+		console.error(err);
 	})
-	.then((response) => {
-		const stream = response.stream();
-
-		stream.on('data', (task) => {
-			if (task.assignee_status === 'today' || 
-				task.assignee_status === 'new') {
-				stream.pause();
-
-				// make an api call to migrate the tasks
-				new Promise((resolve, reject) => {
-					console.log(task);
-					stream.resume();
-				});
-			}
-		}).on('end', () => {
-			console.log('Done transferring the tasks.')
-		}).on('error', (err) => {
-			console.error(err);
-		})
-	})
+})
